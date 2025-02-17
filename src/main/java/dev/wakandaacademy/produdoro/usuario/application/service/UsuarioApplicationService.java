af5d5a8e6175a8ebddac0da93ff003fa882b2fa9
Augusto Andrade <gutoandrade01@gmail.com>
2025-02-17 18:52:34 -0300

    Feat: Implementa camada Service

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationService.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationService.java
@@ -43,5 +43,15 @@ public class UsuarioApplicationService implements UsuarioService {
 		return new UsuarioCriadoResponse(usuario);
 	}
 
+	@Override
+	public void mudaStatusParaPausaLonga(String email, UUID idUsuario){
+		log.info("[inicia] usuarioApplicationService - mudaStatusParaPausaLonga");
+		Usuario usuario = usuarioRepository.buscaUsuarioPorEmail(email);
+		usuarioRepository.buscaUsuarioPorId(idUsuario);
+		usuario.mudaStatusParaPausaLonga(idUsuario);
+		usuarioRepository.salva(usuario);
+		log.info("[finaliza] usuarioApplicationService - mudaStatusParaPausaLonga");	
+	}
+
 
 }
--- src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.Email;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.http.HttpStatus;
 
+import dev.wakandaacademy.produdoro.handler.APIException;
 import dev.wakandaacademy.produdoro.pomodoro.domain.ConfiguracaoPadrao;
 import dev.wakandaacademy.produdoro.usuario.application.api.UsuarioNovoRequest;
 import lombok.AccessLevel;
@@ -41,4 +43,28 @@ public class Usuario {
 		this.status = StatusUsuario.FOCO;
 		this.configuracao = new ConfiguracaoUsuario(configuracaoPadrao);
 	}
+
+	private void pertenceAoUsuario(UUID idUsuario) {
+		if (!this.idUsuario.equals(idUsuario)) {
+			throw APIException.build(HttpStatus.UNAUTHORIZED, "Credencial de autenticação não é válida.");
+		}
+	}
+
+    public void mudaStatusParaPausaLonga(UUID idUsuario){
+		pertenceAoUsuario(idUsuario);
+		validaStatusPausaLonga();
+		mudaStatusPausaLonga();
+	}
+    
+	private void mudaStatusPausaLonga(){
+		this.status=StatusUsuario.PAUSA_LONGA;
+	}
+
+	private void validaStatusPausaLonga(){
+		if(this.status.equals(StatusUsuario.PAUSA_LONGA)){
+			throw APIException.build(HttpStatus.CONFLICT,"Usuário Ja esta em PAUSA LONGA!");
+		}
+	}
 }
+
+
