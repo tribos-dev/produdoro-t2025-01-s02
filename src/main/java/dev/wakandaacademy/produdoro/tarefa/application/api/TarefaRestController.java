1bc436d41d7ba1b9d29445cea85de9e16219da56
Ricardo Lacerda <revricardolacerda@gmail.com>
2025-02-19 06:23:30 -0300

    Implementa camada controller

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
@@ -38,7 +38,8 @@ public class TarefaRestController implements TarefaAPI {
 	@Override
 	public void concluiTarefa(String token, UUID idTarefa) {
 		log.info("[inicia] TarefaRestController --> concluiTarefa");
-
+		String usuarioEmail = getUsuarioByToken(token);
+		tarefaService.concluiTarefa(usuarioEmail, idTarefa);
 		log.info("[finaliza] TarefaRestController --> concluiTarefa");
 	}
 
