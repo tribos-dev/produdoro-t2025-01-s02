3572b916c7b607c9197563869fb87e38146c05ca
Ricardo Lacerda <revricardolacerda@gmail.com>
2025-02-26 08:29:18 -0300

    Corrige erros testes unitários

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -1,26 +1,33 @@
 package dev.wakandaacademy.produdoro.tarefa.application.service;
 
 import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.config.security.service.TokenService;
+import dev.wakandaacademy.produdoro.handler.APIException;
+import dev.wakandaacademy.produdoro.tarefa.application.api.EditaTarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
+import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaUsuarioListReponse;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusAtivacaoTarefa;
 import dev.wakandaacademy.produdoro.tarefa.domain.StatusTarefa;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
 import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static dev.wakandaacademy.produdoro.DataHelper.getTarefaRequest;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
 class TarefaApplicationServiceTest {
@@ -32,8 +39,12 @@ class TarefaApplicationServiceTest {
     // @MockBean
     @Mock
     TarefaRepository tarefaRepository;
+
     @Mock
     UsuarioRepository usuarioRepository;
+
+    @Mock
+    TokenService tokenService;
 
     @Test
     void deveRetornarIdTarefaNovaCriada() {
@@ -48,18 +59,19 @@ class TarefaApplicationServiceTest {
     }
 
     @Test
-
     void deveEditarDescricaoTarefa() {
-        EditaTarefaRequest request = getEditaTarefaRequest();
         Tarefa tarefa = DataHelper.createTarefa();
         Usuario usuario = DataHelper.createUsuario();
+        EditaTarefaRequest editaTarefaRequest = DataHelper.createEditaTarefa();
         when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
         when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
-        tarefaApplicationService.editaTarefa(usuario.getEmail(), tarefa.getIdTarefa(), request);
+        tarefaApplicationService.editaTarefa(usuario.getEmail(), tarefa.getIdTarefa(), editaTarefaRequest);
+        verify(usuarioRepository, times(1)).buscaUsuarioPorEmail(usuario.getEmail());
+        verify(tarefaRepository, times(1)).buscaTarefaPorId(tarefa.getIdTarefa());
+        assertEquals("TAREFA 1", tarefa.getDescricao());
+    }
 
-        assertEquals(request.getDescricao(), tarefa.getDescricao());
-        verify(tarefaRepository, times(1)).salva(tarefa);
-
+    @Test
     void deveConcluirTarefa() {
         Usuario usuario = DataHelper.createUsuario1();
         Tarefa tarefa = DataHelper.createTarefa();
@@ -400,5 +412,4 @@ class TarefaApplicationServiceTest {
         assertEquals(HttpStatus.CONFLICT, exception.getStatusException());
         assertEquals("Usuário não possui tarefa(as) cadastrada(as)", exception.getMessage());
     }
-
 }
